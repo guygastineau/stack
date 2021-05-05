c6bc9fff557fb5ae1b806c24c7635964580861f6
Guy Gastineau <strings.stringsandstrings@gmail.com>
2021-05-04 23:37:29 -0400

    added last simple push test

--- test/test_stack.c
+++ test/test_stack.c
@@ -27,12 +27,37 @@ static void test_push_one(void)
   TEST_ASSERT_NULL_MESSAGE(st, "st wasn't NULL after Stack deallocation");
 }
 
+static void test_push_several(void)
+{
+  Stack st = mkStack();
+  TEST_ASSERT_EQUAL_INT_MESSAGE(1, stPush(&st, 1),
+                                "Failed to push to stack");
+  TEST_ASSERT_EQUAL_size_t_MESSAGE(1, stLength(&st),
+                                   "Stack is incorrect length after pushing");
+
+  TEST_ASSERT_EQUAL_INT_MESSAGE(1, stPush(&st, 2),
+                                "Failed to push to stack");
+  TEST_ASSERT_EQUAL_size_t_MESSAGE(2, stLength(&st),
+                                   "Stack is incorrect length after pushing");
+
+  TEST_ASSERT_EQUAL_INT_MESSAGE(1, stPush(&st, 3),
+                                "Failed to push to stack");
+  TEST_ASSERT_EQUAL_size_t_MESSAGE(3, stLength(&st),
+                                   "Stack is incorrect length after pushing");
+
+  stDestroy(&st, NULL);
+
+  TEST_ASSERT_NULL_MESSAGE(st, "st wasn't NULL after Stack deallocation");
+
+}
+
 int main(void)
 {
    UnityBegin("test/test_stack.c");
 
    RUN_TEST(test_mkStack);
    RUN_TEST(test_push_one);
+   RUN_TEST(test_push_several);
 
    return UnityEnd();
 }

985a3c817d6fbb642badc929c72d7fd172862853
Guy Gastineau <strings.stringsandstrings@gmail.com>
2021-05-04 22:59:55 -0400

    add the declarations to the implementation file

--- src/stack.c
+++ src/stack.c
@@ -1,1 +1,16 @@
 #include "stack.h"
+
+// Add a definition for struct Node here.
+
+// This should really just return `NULL'
+Stack mkStack();
+
+int stPush(Stack *st, SData data);
+
+// Non-destructive inspection.
+int stPeek(Stack *st, SData *dest);
+
+size_t stLength(Stack *st);
+
+// Destroys the head of the list.
+int stPop(Stack *st, SData *dest);
